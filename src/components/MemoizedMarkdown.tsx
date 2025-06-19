383478e5039d7a624061de6dc459a84d4c614955
star_sailor <71993095+Cinnamonsroll@users.noreply.github.com>
2025-06-19 17:46:10 -0400

    Fix useState import

--- src/components/MemoizedMarkdown.tsx
+++ src/components/MemoizedMarkdown.tsx
@@ -1,5 +1,5 @@
 import { marked } from 'marked'
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
